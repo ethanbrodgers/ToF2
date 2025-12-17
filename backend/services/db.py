e683489c3e545fdaa13cf9965c4825f2c6e5218a
Jerry Chen <184151454+jerrychen751@users.noreply.github.com>
2025-12-17 14:17:42 -0600

    fix: remove linting error by checking MONGODB_DB_NAME existence in .env

--- backend/services/db.py
+++ backend/services/db.py
@@ -4,5 +4,7 @@ from pymongo import MongoClient
 import os
 
 client = MongoClient(os.getenv("MONGODB_URI"))
-db = client[os.getenv("MONGODB_DB_NAME")]
-# db = client["ToF2-database"]
+db_name = os.getenv("MONGODB_DB_NAME")
+if not db_name:
+    raise ValueError("Make sure to set 'MONGODB_DB_NAME' environment variable.")
+db = client[db_name] # Ex: db = client["ToF2-database"]
