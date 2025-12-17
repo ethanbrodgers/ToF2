ed08f840d3e544d9aa631022b5c45e826c92402b
Jerry Chen <184151454+jerrychen751@users.noreply.github.com>
2025-12-17 13:33:52 -0600

    feat: add url prefixing when registering blueprints

--- backend/routing/register_all_bp.py
+++ backend/routing/register_all_bp.py
@@ -1,15 +1,17 @@
 from .blueprints.vocab import bp as vocab_bp
 from .blueprints.grammar import bp as grammar_bp
+from .blueprints.style import bp as style_bp
 from .blueprints.welcome import bp as welcome_bp
 from .blueprints.error import bp as error_bp
 
 def register_all_bp(app):
     """Registers all blueprints to the given Flask app, giving it all endpoints created in the routing package"""
     bps = [
-        vocab_bp,
-        welcome_bp,
-        error_bp,
-        grammar_bp
+        (vocab_bp, "/vocab"),
+        (welcome_bp, "/welcome"),
+        (error_bp, "/error"),
+        (grammar_bp, "/grammar"),
+        (style_bp, "/style"),
     ]
-    for bp in bps:
-        app.register_blueprint(bp)
+    for bp, prefix in bps:
+        app.register_blueprint(bp, url_prefix=prefix)
