43307c3d2ec8f265e542fc14194afd3dcbe00e89
ethanbrodgers <ethanbrodgers@gmail.com>
2025-12-17 14:13:07 -0500

    Added error logging to frontend API service; improved "key" warning suppressing

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -8,7 +8,7 @@ import { wordType, ruleNormType } from "../types";
 
 // ===== Internal use =====
 
-const BACKEND_URL = "http://127.0.0.1:5000/";
+const BACKEND_URL = "http://localhost:5000";
 
 // backend call logic: Input all info for request, outputs promise resolving to response object
 async function apiCall(
@@ -18,6 +18,7 @@ async function apiCall(
     queryParams: {[key: string]: string},
     pathVar: string
 ): Promise<Response> {
+    // construct request
     let urlString = BACKEND_URL + endpoint;
     if (pathVar) urlString += "/" + pathVar;
     if (queryParams) {
@@ -32,7 +33,23 @@ async function apiCall(
         body: undefined
     };
     if (body && httpMethod != "GET") configObj.body = JSON.stringify(body);
-    return fetch(urlString, configObj);
+
+    // make request
+    const response = await fetch(urlString, configObj)
+
+    // log errors
+    if (!response.ok) {
+        console.log("Errored backend call");
+        console.log("endpoint:", endpoint);
+        console.log("httpMethod:", httpMethod);
+        console.log("body:", body);
+        console.log("queryParams:", queryParams);
+        console.log("pathVar:", pathVar);
+        console.log("response:", response);
+    }
+
+    // return
+    return response;
 }
 
 
@@ -42,13 +59,13 @@ async function apiCall(
 // ===== Exports =====
 
 export async function getWords(lang: string): Promise<Array<wordType>> {
-    return apiCall("vocab", "GET", null, {lang: lang}, null)
+    return apiCall("/vocab", "GET", null, {lang: lang}, null)
     .then(response => response.json())
     .then(response => response.words)
 }
 
 export async function getRules(lang: string): Promise<Array<ruleNormType>> {
-    return apiCall("grammar", "GET", null, {lang: lang}, null)
+    return apiCall("/grammar", "GET", null, {lang: lang}, null)
     .then(response => response.json())
     .then(response => response.rules)
 }
