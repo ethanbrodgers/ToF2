6f69a0c7592876106b7b5a4898d2d3f65d8b838c
ethanbrodgers <ethanbrodgers@gmail.com>
2025-12-18 11:42:43 -0500

    feat: made ExSentence component display "positive" field by making text red or green

--- frontend/src/pages/view/components/ExSentence.tsx
+++ frontend/src/pages/view/components/ExSentence.tsx
@@ -9,9 +9,9 @@
  * 
  * @param {string} props.children.targ - example sentence in the target language
  */
-export default function ExSentence({children: {en, targ} }: {children: {en: string, targ: string}, key?: any}) {
+export default function ExSentence({children: {en, targ, positive} }: {children: {en: string, targ: string, positive?: boolean}, key?: any}) {
     return ( <div>
-        <p className="text-green-400">{targ}</p>
+        <p className={(positive === false) ? "text-red-400" : "text-green-400"}>{targ}</p>
         <p>{en}</p>
     </div> );
 }
