0173da36fe4101c64be91e7900b5da212d17ee90
ethanbrodgers <ethanbrodgers@gmail.com>
2025-12-17 21:34:53 -0500

    Added add-word endpoint (PUT /vocab)

--- backend/routing/blueprints/vocab.py
+++ backend/routing/blueprints/vocab.py
@@ -30,6 +30,55 @@ def word_lookup_query_param():
     # return
     return jsonify({"words": words})
 
+# add a word
+@bp.route("/vocab", methods=["PUT"])
+def add_word():
+    req_body = request.get_json()
+
+    # input validation
+    if not req_body.get("lang"):
+        return jsonify({"message": "No lang field provided"}), 400
+    if type(req_body["lang"]) != str:
+        return jsonify({"message": "Received a non-string lang field"}), 400
+    if len(req_body["lang"]) != 2:
+        return jsonify({"message": "Received a lang field with length != 2"}), 400
+    if not req_body.get("en"):
+        return jsonify({"message": "No en field provided"}), 400
+    if type(req_body["en"]) != str:
+        return jsonify({"message": "Received a non-string en field"}), 400
+    if not req_body.get("targ"):
+        return jsonify({"message": "No targ field provided"}), 400
+    if type(req_body["targ"]) != str:
+        return jsonify({"message": "Received a non-string targ field"}), 400
+    if not req_body.get("pos"):
+        return jsonify({"message": "No pos field provided"}), 400
+    if req_body["pos"] not in ["n", "p", "v", "adj", "adv", "c", "i", "q"]:
+        return jsonify({"message": "Received a pos field not in recognized list"}), 400
+    
+    # construct word
+    word = {}
+    word["en"] = req_body["en"]
+    word["targ"] = req_body["targ"]
+    word["lang"] = req_body["lang"]
+    word["pos"] = req_body["pos"]
+    word["def"] = req_body.get("def") if req_body.get("def") else "[none provided]"
+    word["gender"] = req_body.get("gender") if req_body.get("gender") else None
+    word["trans"] = req_body.get("trans") if req_body.get("trans") else None
+    
+    if req_body.get("desc"):
+        word_desc = {}
+        req_desc = req_body["desc"]
+        word_desc["text"] = req_desc.get("text") if req_desc.get("text") else []
+        word_desc["ex"] = req_desc.get("ex") if req_desc.get("ex") else []
+        word["desc"] = word_desc
+
+    # add
+    db["Words"].insert_one(word)
+
+    # return
+    return jsonify({"message": "success"})
+
+
 # delete word
 @bp.route("/vocab/<id_str>", methods=["DELETE"])
 def delete_word(id_str):
