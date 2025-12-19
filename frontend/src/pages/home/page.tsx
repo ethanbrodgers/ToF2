f1c23531ef7937fdb01df17f5e7f911052b33b27
ethanbrodgers <ethanbrodgers@gmail.com>
2025-12-19 16:46:04 -0500

    feat: dummy homepage, just the word "homepage" and a navbar at the frontend / path for quick navigation to other pages

---
+++ frontend/src/pages/home/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from '@/components/Navbar';
+
+/**
+ * The homepage for the website, used for navigation to other features on opening the site.
+ */
+export default function Homepage() {
+    return ( <div>
+        <Navbar />
+        <p>This is the homepage</p>
+    </div> );
+}
+
